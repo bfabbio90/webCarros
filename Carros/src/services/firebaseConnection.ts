176057a5ec962049d1ec0d5537e671b8a55030b6
Bruno Fabbio <bruno@brunofabbiodev.com>
2025-05-04 21:36:08 -0300

    feat: add dockerfile for getting the dependencies ready

---
+++ Carros/src/services/firebaseConnection.ts
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  /* Sua API e seus domínios do Firebase */
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, auth, storage };
